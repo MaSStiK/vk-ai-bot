b3bba49a79e5d737eda017cf0de4cddcd4a2eeca
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-06-04 16:52:44 +0200

    answer if bot mention

--- main.py
+++ main.py
@@ -31,14 +31,7 @@ def start_typing_loop(vk, peer_id, stop_event):
             log_error(f"Ошибка при setActivity: {e}")
         time.sleep(4)
 
-def main():
-    session = vk_api.VkApi(token=VK_TOKEN)
-    vk = session.get_api()
-    longpoll = VkLongPoll(session)
-    user_histories = load_memory()
-    print("Бот запущен")
-
-    def vk_messages_send(peer_id, message, reply_to=None):
+def vk_messages_send(vk, peer_id, message, reply_to=None):
         params = {
             "peer_id": peer_id,
             "random_id": 0,
@@ -47,6 +40,13 @@ def main():
         if reply_to:
             params["reply_to"] = reply_to
         vk.messages.send(**params)
+
+def main():
+    session = vk_api.VkApi(token=VK_TOKEN)
+    vk = session.get_api()
+    longpoll = VkLongPoll(session)
+    user_histories = load_memory()
+    print("Бот запущен")
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
@@ -62,7 +62,7 @@ def main():
                 try:
                     if user_id > 0:
                         user = vk.users.get(user_id=user_id)[0]
-                        user_name = f"{user['first_name']} {user['last_name']}"
+                        user_name = f"{user["first_name"]} {user["last_name"]}"
                     else:
                         group = vk.groups.getById(group_id=-user_id)[0]
                         user_name = group["name"]
@@ -70,64 +70,81 @@ def main():
                 except:
                     user_name = f"ID {user_id}"
 
-            if not from_me and "reply" in event.attachments:
-                try:
-                    reply_data = json.loads(event.attachments['reply'])
-                    reply_conv_id = reply_data.get("conversation_message_id")
-                    response = vk.messages.getByConversationMessageId(
-                        peer_id=peer_id,
-                        conversation_message_ids=reply_conv_id
-                    )
-                    replied_message = response["items"][0]
+            if not from_me:
+                generate_answer = False # Проверка подходит ли сообщение для ответа
+                
+                # Проверяем если в сообщении ответ на сообщение бота 
+                if "reply" in event.attachments: 
+                    try:
+                        reply_data = json.loads(event.attachments["reply"])
+                        reply_conv_id = reply_data.get("conversation_message_id")
+                        response = vk.messages.getByConversationMessageId(
+                            peer_id=peer_id,
+                            conversation_message_ids=reply_conv_id
+                        )
+                        replied_message = response["items"][0]
 
-                    if replied_message["from_id"] == BOT_ID:
-                        print(f"Генерация ответа на сообщение: \"{user_name}: {message}\"")
+                        # Пропускаем сообщение если в ответе нету бота
+                        if (replied_message["from_id"] == BOT_ID):
+                            generate_answer = True
+                    except Exception as e:
+                        log_error(f"Ошибка обработки reply: {e}")
 
-                        # Инициализируем историю пользователя, если её нет
-                        if str(user_id) not in user_histories:
-                            user_histories[str(user_id)] = []
+                # Если сообщение начинается с уведомления бота
+                if message.startswith(f"[id{BOT_ID}|"):
+                    generate_answer = True
 
-                        # Добавляем новое сообщение в историю
-                        user_histories[str(user_id)].append({"role": "user", "content": f"{user_name}: {message}"})
+                # Если сообщение по условиям подходит к ответу - генерируем ответ
+                if generate_answer: 
+                    print(f"Генерация ответа на сообщение: \"{user_name}: {message}\"")
 
-                        # Обрезаем старую историю при необходимости
-                        if len(user_histories[str(user_id)]) > MAX_HISTORY_LENGTH:
-                            user_histories[str(user_id)] = user_histories[str(user_id)][-MAX_HISTORY_LENGTH:]
+                    # Инициализируем историю пользователя, если её нет
+                    if str(user_id) not in user_histories:
+                        user_histories[str(user_id)] = []
 
-                        stop_typing = threading.Event()
-                        typing_thread = threading.Thread(target=start_typing_loop, args=(vk, peer_id, stop_typing))
-                        typing_thread.start()
+                    # Добавляем новое сообщение в историю
+                    user_histories[str(user_id)].append({"role": "user", "content": f"{user_name}: {message}"})
 
-                        try:
-                            ai_response = client.chat.completions.create(
-                                model="deepseek/deepseek-chat-v3-0324:free",
-                                messages=[{"role": "user", "content": base_prompt}, *user_histories[str(user_id)]]
-                            )
-                            answer = ai_response.choices[0].message.content.strip()
-                            answer = answer.replace("*", "") # Убираем лишний символ
-                            print(f"Ответ нейросети: {answer}")
+                    # Обрезаем старую историю при необходимости
+                    if len(user_histories[str(user_id)]) > MAX_HISTORY_LENGTH:
+                        user_histories[str(user_id)] = user_histories[str(user_id)][-MAX_HISTORY_LENGTH:]
 
-                            # Добавляем ответ ИИ в историю
-                            user_histories[str(user_id)].append({"role": "assistant", "content": answer})
-                            save_memory(user_histories)
+                    stop_typing = threading.Event()
+                    typing_thread = threading.Thread(target=start_typing_loop, args=(vk, peer_id, stop_typing))
+                    typing_thread.start()
 
-                            if len(answer) >= 4000:
-                                print(f"Ответ больше 4000 символов")
-                                answer = answer[:4000]
-                            vk_messages_send(peer_id=peer_id, message=answer, reply_to=message_id)
-                        except:
-                            # Если ошибка [913] Too many forwarded messages - отвечаем без ответа на сообщение
+                    try:
+                        ai_response = client.chat.completions.create(
+                            model="deepseek/deepseek-chat-v3-0324:free",
+                            messages=[{"role": "user", "content": base_prompt}, *user_histories[str(user_id)]]
+                        )
+                        answer = ai_response.choices[0].message.content.strip()
+                        answer = answer.replace("*", "") # Убираем лишний символ
+                        print(f"Ответ нейросети: {answer}")
+
+                        # Добавляем ответ ИИ в историю
+                        user_histories[str(user_id)].append({"role": "assistant", "content": answer})
+                        save_memory(user_histories)
+
+                        if len(answer) >= 4000:
+                            print(f"Ответ больше 4000 символов")
+                            answer = answer[:4000]
+                        vk_messages_send(vk=vk, peer_id=peer_id, message=answer, reply_to=message_id)
+                    except Exception as e:
+                        if e.code == 913: # Если ошибка [913] Too many forwarded messages - отвечаем без ответа на сообщение
                             try:
-                                vk_messages_send(peer_id=peer_id, message=answer)
+                                vk_messages_send(vk=vk, peer_id=peer_id, message=answer)
                             except Exception as e:
-                                log_error(f"Ошибка обработки сообщения: {e}")
-                        finally:
-                            stop_typing.set()
-                            typing_thread.join()
+                                log_error(f"Ошибка отправки сообщения: {e}")
 
-                except Exception as e:
-                    log_error(f"Ошибка обработки reply: {e}")
-
+                        elif e.code == 429: # Ошибка если закончились токен
+                            log_error(f"Ошибка, закончились токены: {e}")
+                        else:
+                            traceback.print_exc()
+                        
+                    finally:
+                        stop_typing.set()
+                        typing_thread.join()
 
 if __name__ == "__main__":
     while True:
